6acdfead5a14b28aeda085b197509fcb0cb46d83
HixS <maximweiss57@gmail.com>
2023-08-06 22:12:11 +0300

    new GUI + login page and routes

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask,render_template
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 @app.route('/')
-def start():
+def index():
     return render_template('index.html')
+
+@app.route('/index.html')
+def index2():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def do_login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == 'admin' and password == 'password':
+        return redirect('/')
+    else:
+        return render_template('login.html', error='Invalid username or password.')
