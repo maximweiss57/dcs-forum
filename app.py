ce493c6402660cc4d0b3cb6c25badd7bf48b104d
maxim <maximweiss57@gmail.com>
2023-08-08 12:30:03 +0200

    functional login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -7,35 +7,48 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dcs-forum.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY']='dcs-forum'
+app.config['SECRET_KEY'] = 'dcs-forum'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-   
-with app.app_context():
-    db.create_all()
 
-    def __repr__(self):
-        return '<Users %r>' % self.id
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_user=current_user)
 
 @app.route('/index.html')
 def index2():
-    return render_template('index.html')
+    return render_template('index.html', current_user=current_user)
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                login_user(user)
+                return redirect('/index.html')
+            else:
+                return 'Wrong password'
+        else:
+            return 'User not found'
+    else:
+        return render_template('login.html')
 
-@app.route('/register',methods=['POST','GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -46,21 +59,30 @@ def register():
             db.session.add(user)
             db.session.commit()
         except:
-            return 'There was an issue with registration proccess'
+            return 'There was an issue with the registration process'
         return redirect('/login')
     else:
         return render_template('register.html')
-    
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/index.html')
+
 @app.route('/forums')
 def forums():
-    return render_template('forums.html')
+    return render_template('forums.html', current_user=current_user)
+
 @app.route('/downloads')
 def downloads():
-    return render_template('downloads.html')
+    return render_template('downloads.html', current_user=current_user)
+
 @app.route('/events')
 def events():
-    return render_template('events.html')
+    return render_template('events.html', current_user=current_user)
+
 @app.route('/squadrons')
 def squadrons():
-    return render_template('squadrons.html')
+    return render_template('squadrons.html', current_user=current_user)
 
