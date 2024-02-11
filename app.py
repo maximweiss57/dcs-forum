8c3c80f8ff3aa9f0ebabd4240d5c46481b6d77a9
Maxim weiss <137171905+maximweiss57@users.noreply.github.com>
2024-02-11 08:35:09 +0200

    Update Dockerfile-db, run.py, Dockerfile-app, and templates/base.html, and add search_results.html and profile.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint,url_for
+from flask import Flask, Blueprint
 from flask_login import LoginManager
 from models import Users
 from instance import db
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
 
-app=create_app(testing=False)
+app=create_app(testing=True)
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
